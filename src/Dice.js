96f4dc32512d10db81fe4b6e4c66dff5f23e2f8e
Albert Stjärne <albert.stjarne@gmail.com>
2019-09-05 12:57:48 +0200

    adds fontawesome cdn, adds random number to represent dice roll

--- src/Dice.js
+++ src/Dice.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class Dice extends Component {
   render() {
-    return <h2>Dice Component</h2>;
+    let rand = Math.floor(Math.random() * 6) + 1;
+    return (
+      <div>
+        <h2>Dice Component</h2>
+        {rand}
+        <i className="fas fa-dice-three"></i>
+      </div>
+    );
   }
 }
 
